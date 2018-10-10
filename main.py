f96e9a1a8c94a9cc677878cc7555fab4a13decae
Victor Marquez <marquezvictor1599@gmail.com>
2018-10-10 16:27:18 -0400

    Yessssss Done

--- main.py
+++ main.py
@@ -1,27 +1,70 @@
-from flask import Flask, request
+from flask import Flask, request, redirect, render_template
+import re
+
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-username = """
-<!doctype html>
-<html>
-    <body>
-        <form action="/hello" method="post">
-            <label for="username">username:</label>
-            <input id="username" type="text" name="user_name" />
-            <input type="submit" />
-        </form> 
-    </body>
-</html>
-"""
-
-@app.route("/")
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    return username
 
-@app.route("/hello", methods=['POST'])
-def hello():
-    user_name = request.form['user_name']
-    return '<h1>Hello, ' + user_name + '<h1>'
+    username = ''
+    email = ''
+    username_error = ''
+    password_error = ''
+    verify_password_error = ''
+    email_error = ''
+    title = 'Signup'
 
-app.run()
+    if request.method == 'POST':
+
+        username = request.form['username']
+        password = request.form['password']
+        verify_password = request.form['verify_password']
+        email = request.form['email']
+
+        for page in username:
+            if page.isspace():
+                username_error = 'No spaces between words.'
+                username = ''
+            else:
+                if (len(username) < 3) or (len(username) > 20):
+                    username_error = 'Username needs to be 3-20 characters.'
+                    username = ''
+
+        if not username:
+            username_error = 'Not a valid username'
+            username = ''
+
+        for page in password:
+            if page.isspace():
+                password_error = 'No spaces between words.'
+            else:
+                if (len(password) < 3) or (len(password) > 20):
+                    password_error = 'Password must be 3-20 characters and not spaces between words.'
+        if not len(password):
+            password_error = 'Not a valid password'
+
+        if password != verify_password:
+            verify_password_error = 'Passwords do not match.'
+
+        if (email != '') and (not re.match('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$', email)):
+            email_error = 'This is not a valid email.'
+            email = ''
+
+        if (not username_error) and (not password_error) and (not verify_password_error) and (not email_error):
+            return redirect('/start?username={0}'.format(username))
+
+    return render_template('home.html', title=title, username=username, email=email,
+                           username_error=username_error, password_error=password_error,
+                           verify_password_error=verify_password_error, email_error=email_error)
+
+@app.route('/start')
+def start():
+    title = "Welcome"
+    username = request.args.get('username')
+    return render_template('start.html', title=title, username=username)
+
+if __name__ == '__main__':
+    app.run()
+
+#Victor Marquez
